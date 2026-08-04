2b20ce479c73b73cb1383511d6bc0eabc664d842
Kai Matton <kdmatton42@tntech.edu>
2026-08-04 14:55:56 -0500

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        numsMap ={}
+        for i in range(len(nums)):
+            if nums[i] not in numsMap:
+                numsMap[nums[i]] = None
+            else:
+                return True
+        return False
